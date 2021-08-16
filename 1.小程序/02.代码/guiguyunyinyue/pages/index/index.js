d7a35e497bf3c62a01c6ad928f313fadba2f53ef
HotJuly <949588380@qq.com>
2021-08-16 11:56:36 +0800

    27.首页模块请求数据

--- 1.小程序/02.代码/guiguyunyinyue/pages/index/index.js
+++ 1.小程序/02.代码/guiguyunyinyue/pages/index/index.js
@@ -4,14 +4,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    // 该数据用于首页轮播图展示
+    banners:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    /*
+      1.在哪发
+        Vue中一般是在created或者mounted中发送请求
+      2.怎么发
+        注意:小程序中window没有值,全局对象是wx
+        API:wx.request(Object object)
+      3.往哪发
+        查看接口文档
+        需要注意的内容:
+          1.接口地址
+          2.请求方式
+          3.请求参数
+     */
+    // console.log('window', window)
+    console.log(1)
+    wx.request({
+      url:"http://localhost:3000/banner",
+      data:{
+        type:2
+      },
+      success:(res)=>{
+        // console.log('res',res)
+        this.setData({
+          banners:res.data.banners
+        })
+      }
+    })
+    console.log(2)
   },
 
   /**
