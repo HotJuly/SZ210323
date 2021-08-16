0db5e4e0bd9bf7ab5278daa4262c1a77c059f008
HotJuly <949588380@qq.com>
2021-08-16 17:01:41 +0800

    32.首页模块排行榜区域静态样式

--- 1.小程序/02.代码/guiguyunyinyue/utils/req.js
+++ 1.小程序/02.代码/guiguyunyinyue/utils/req.js
@@ -8,6 +8,10 @@
     2.将内部每次调用都不相同的内容,通过形参传入
     3.谁调用,谁传入
 
+  封装组件
+    1.保留重复出现的代码片段(wxml+wxss+部分js)
+    2.将内部每次展示的不同内容,通过标签属性向内部传入数据
+    3.谁使用,谁传入
  */
 import config from './config.js';
 export default function (url, data={}, method="GET") {
