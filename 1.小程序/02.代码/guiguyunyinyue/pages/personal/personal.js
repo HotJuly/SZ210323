0d6b5a07cffdebb76a03b4a645ad9cf4395126ba
HotJuly <949588380@qq.com>
2021-08-17 11:19:44 +0800

    C3效果wxs版本

--- 1.小程序/02.代码/guiguyunyinyue/pages/personal/personal.js
+++ 1.小程序/02.代码/guiguyunyinyue/pages/personal/personal.js
@@ -5,7 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
+    moveDistance:0,
+    moveTransition:""
+  },
 
+  // 用于监视用户手指按下事件
+  handleTouchStart(event){
+    // console.log('handleTouchStart', event)
+    this.startY  = event.touches[0].clientY;
+    // console.log(startY)
+    this.setData({
+      moveTransition:""
+    })
+  },
+
+  // 用于监视用户手指移动事件
+  handleTouchMove(event) {
+    // console.log('handleTouchMove', event)
+    let moveY = event.touches[0].clientY;
+    let moveDistance = moveY - this.startY;
+    if(moveDistance>80 || moveDistance<0)return;
+    this.setData({
+      moveDistance
+    })
+  },
+
+  // 用于监视用户手指抬起事件
+  handleTouchEnd(event) {
+    // console.log('handleTouchEnd', event)
+    this.setData({
+      moveDistance:0,
+      moveTransition:"transform 1s"
+    })
   },
 
   /**
