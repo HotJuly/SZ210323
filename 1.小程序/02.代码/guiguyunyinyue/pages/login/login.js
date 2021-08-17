ee4566dea76932032abb7d17edc21a26369b74d2
HotJuly <949588380@qq.com>
2021-08-17 15:06:09 +0800

    41.登录界面登录交互实现

--- 1.小程序/02.代码/guiguyunyinyue/pages/login/login.js
+++ 1.小程序/02.代码/guiguyunyinyue/pages/login/login.js
@@ -1,4 +1,5 @@
 // pages/login/login.js
+import req from '../../utils/req.js';
 Page({
 
   /**
@@ -31,10 +32,11 @@ Page({
     /*
       小程序组件向事件回调函数内部传参
         解决:通过组件的标签属性,可以向事件回调函数内部传参
-    
+      event.target就是最内层的子元素,即是触发者
+      event.currentTarget就是绑定事件监听的事件源
      */
     // console.log('handleInput', event.target.dataset.type)
-    console.log('handleInput', event.target)
+    // console.log('handleInput', event.target)
     let type = event.target.dataset.type;
     let value = event.detail.value;
     this.setData({
@@ -43,6 +45,90 @@ Page({
     // return function () {
     //   console.log('handleInput')
     // }
+  },
+
+  // 监视用户点击登录按钮,发送登录请求
+  async handleLogin(){
+    // console.log('handleLogin')
+    // 1.收集数据(已经收集在data中了)
+    const {phone,password} = this.data;
+
+    // 2.前台表单验证
+    if(!phone.trim()){
+      //弹窗提示用户,请输入手机号,并且逻辑到此为止
+      wx.showToast({
+        title:"手机号不能为空,请输入手机号",
+        icon:"none"
+      })
+      return;
+    }
+
+    if (!password.trim()) {
+      //弹窗提示用户,请输入密码,并且逻辑到此为止
+      wx.showToast({
+        title: "密码不能为空,请输入密码",
+        icon: "none"
+      })
+      return;
+    }
+
+    // 3.发送请求
+    /*
+      状态码情况:
+        1.帐号错误 400
+        2.密码错误 502
+        3.登陆成功 200
+     */
+    let result = await req('/login/cellphone',{
+      phone,
+      password
+    });
+
+    console.log('result',result)
+
+    // 4.成功做什么
+    // 提示用户登录成功,并回到个人中心页面
+    // if(result.code===200){
+    //   // 登陆成功
+    // }else if(result.code===400){
+    //   // 帐号错误
+    // } else if (result.code === 502) {
+    //   // 密码错误
+    // }
+
+    // 5.失败做什么
+    // 提示用户失败信息
+    const code = result.code;
+    const codeFn = {
+      200(){
+        // 该函数用于登录成功状态
+        // console.log('登录成功')
+        wx.showToast({
+          icon: "none",
+          title: '登录成功,即将跳转',
+        });
+        wx.switchTab({
+          url:"/pages/personal/personal"
+        })
+      },
+      400() {
+        // 该函数用于帐号错误状态
+        console.log('帐号错误')
+        wx.showToast({
+          icon: "none",
+          title: '帐号错误,请检查',
+        });
+      },
+      502() {
+        // 该函数用于密码错误状态
+        // console.log('密码错误')
+        wx.showToast({
+          icon:"none",
+          title: '密码错误,请检查',
+        });
+      }
+    }
+    codeFn[code]&&codeFn[code]();
   },
 
   /**
