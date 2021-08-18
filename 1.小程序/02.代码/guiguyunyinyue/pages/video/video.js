832236083c67aca780bae4d232c98a77ee0d8f4d
HotJuly <949588380@qq.com>
2021-08-18 12:16:17 +0800

    52.小程序转发功能

--- 1.小程序/02.代码/guiguyunyinyue/pages/video/video.js
+++ 1.小程序/02.代码/guiguyunyinyue/pages/video/video.js
@@ -12,8 +12,21 @@ Page({
     videoList:[]
   },
 
+  async getVideoList(){
+    //用于请求视频列表数据
+    let videoListData = await req('/video/group', { id: this.data.currentId });
+    let videoList = videoListData.datas.map((item) => {
+      return item.data;
+    })
+    this.setData({
+      videoList
+    })
+
+    console.log('1')
+  },
+
   // 用于监视用户点击导航栏操作,并动态切换导航栏下划线展示
-  changeNav(event){
+  async changeNav(event){
     // target和currentTarget的使用,根据自定义属性存放的位置决定
     // 如果当前数据放在最内层目标对象身上,就是用event.target获取
     // 如果当前数据放在事件源身上,就是用event.currentTarget获取
@@ -21,8 +34,16 @@ Page({
     // console.log('currentId', currentId)
     let currentId = event.currentTarget.dataset.id;
     this.setData({
-      currentId
+      currentId,
+      videoList:[]
     })
+
+    wx.showLoading({
+      title:"正在加载,请稍等"
+    })
+    await this.getVideoList();
+    console.log('2')
+    wx.hideLoading({});
   },
 
   /**
@@ -50,14 +71,8 @@ Page({
       currentId: navListData.data[0].id
     });
 
-    //用于请求视频列表数据
-    let videoListData = await req('/video/group',{id:this.data.currentId});
-    let videoList = videoListData.datas.map((item)=>{
-      return item.data;
-    })
-    this.setData({
-      videoList
-    })
+    // 封装请求视频列表函数
+    this.getVideoList();
     // console.log('videoList', videoList)
   },
 
@@ -92,7 +107,33 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function ({ from,target}) {
+    console.log('onShareAppMessage', from, target)
+    /*
+      当用户点击右上角转发按钮,应该转发的内容是小程序logo,小程序名称
+      当用户点击某个button按钮进行转发,应该转发的内容是当前视频图片和名称
+      问题:如何区分用户触发当前事件监听的渠道
+      解决:可以通过形参中的from属性,判断用户触发的渠道
+      
+      问题:如何自定义转发内容
+      解决:通过return一个对象,可以实现转发内容的自定义效果
+    */
+    if(from==="menu"){
+      // 说明用户是点击右上角转发进入的
+      return {
+        title:"硅谷云音乐",
+        imageUrl:"/static/images/dazuo.jpeg",
+        page:"/pages/index/index"
+      }
+    } else if (from === "button") {
+      // 说明用户是点击button按钮进入的
+      // 注意:自定义属性的属性名只能是小写,大写也会默认转为小写
+      const {title,imageurl} = target.dataset;
+      return {
+        title,
+        imageUrl: imageurl,
+        page: "/pages/video/video"
+      }
+    }
   }
 })
