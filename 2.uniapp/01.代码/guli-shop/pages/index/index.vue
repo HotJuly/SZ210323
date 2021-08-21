e2d78c83a57704e2f75aef85824d41b4e75e096f
HotJuly <949588380@qq.com>
2021-08-21 12:11:23 +0800

    6.首页头部静态样式实现

---
+++ 2.uniapp/01.代码/guli-shop/pages/index/index.vue
@@ -0,0 +1,63 @@
+<template>
+	<!-- 
+		uniapp既可以使用小程序组件,也可以使用html标签
+		但是,推荐使用小程序组件
+	 -->
+	<view class="indexContainer">
+		<view class="header">
+			<image class="logo" src="../../static/images/logo.png" mode=""></image>
+			<view class="search">
+				<i class="iconfont icon-sousuo"></i>
+				<input type="text" placeholder-class="placeholder"	placeholder="搜索商品" class="searchInput" value="" />
+			</view>
+			<button class="username">七月</button>
+		</view>
+	</view>
+	<!-- swiper
+	<div>indexContainer</div> -->
+	<!-- <div>
+		<i>123</i><i>234</i>
+	</div> -->
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style lang="stylus">
+	.indexContainer
+		.header
+			padding-top 20rpx
+			display flex
+			align-items  center
+			.logo
+				width 118rpx
+				height 40rpx
+				margin 0 20rpx
+				flex-shrink  0
+			.search
+				position relative
+				background  #ccc
+				border-radius  10rpx
+				height 60rpx
+				flex-grow 1
+				padding-left 80rpx
+				.iconfont
+					position absolute
+					left 20rpx
+					top 50%
+					transform translateY(-50%)
+				.searchInput
+					height 100%
+				.placeholder
+					text-align center
+					font-size 28rpx
+					text-indent -80rpx
+			.username
+				width 140rpx
+				height 60rpx
+				font-size 24rpx
+				color red
+				flex-shrink  0
+				margin 0 20rpx
+</style>
--- 2.uniapp/01.代码/uniapp-init/main.js
+++ 2.uniapp/01.代码/uniapp-init/main.js
@@ -8,7 +8,17 @@ Vue.config.productionTip = false
 // 说明,当前App组件代表整个小程序
 App.mpType = 'app'
 
+// 相当于小程序app.js中调用App()注册整个小程序
 const app = new Vue({
     ...App
+	// onLaunch: function() {
+	// 	console.log('App Launch')
+	// },
+	// onShow: function() {
+	// 	console.log('App Show')
+	// },
+	// onHide: function() {
+	// 	console.log('App Hide')
+	// }
 })
 app.$mount()
