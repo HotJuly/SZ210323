1c9ec3565e93ec3e2ab166c5195651ce70595463
HotJuly <949588380@qq.com>
2021-09-06 16:00:35 +0800

    39.MVVM源码(数据代理和数据劫持)

--- 3.复习/01.课件/Vue/Vue源码/js/mvvm.js
+++ 3.复习/01.课件/Vue/Vue源码/js/mvvm.js
@@ -1,20 +1,71 @@
 function MVVM(options) {
+  /*
+    options =>
+      {
+        el: "#app",
+        data: {
+          msg: "hello mvvm",
+          person:{
+            name:"xiaoming",
+            msg:"123"
+          },
+        }
+      }
+
+      this是谁? => vm实例对象
+  */
+
    //把配置对象保存给vm一份
     this.$options = options;
     //把配置对象的data保存到vm的_data 地址一样
     var data = this._data = this.$options.data;
+    // 将data对象的地址值保存在三个地方
+    // var data = (this._data = this.$options.data);
+
+
     var me = this; //保存vm给me
 
-    // 数据代理
+    // 1.数据代理
     // 实现 vm.xxx -> vm._data.xxx
+    /*
+      数据代理
+      目的:方便开发者读取$data身上的属性,可以减少一层属性书写(this.$data.msg,可以写作this.msg)
+      总结:数据代理只会代理data中的直系属性,不会深度代理
+      他并不属于响应式原理的一部分,有他没他都一样
+    */
     Object.keys(data).forEach(function(key) {
       // 遍历data当中的所有属性，每个属性都会调用下面的_proxy方法
         me._proxy(key);
     });
 
+    
+    // ["msg"].forEach(function(key) {
+    //   // 遍历data当中的所有属性，每个属性都会调用下面的_proxy方法
+    //     vm._proxy("msg");
+    // });
+
 
     //数据监视劫持（原始data当中的所有属性数据进行监视）
+    /*
+      什么是响应式?
+        当data中的属性值发生修改,页面会重新进行渲染
+        拆解:
+          1.如何知道data中的属性值发生变化
+            其实和数据代理原理相同
+            Vue可以通过Object.defineProperty中的set方法,可以监视到开发者对data中属性值的修改操作
+            此部分通过数据劫持实现
+
+          2.如何导致页面重新渲染
+
+      2.数据劫持
+        数据劫持是在重写$data对象身上的所有的属性,将他从value改为get/set方法
+        data对象身上具有多少个属性,就会被数据劫持多少次
+        注意:所谓的数据劫持,劫持的是属性名,不是属性值
+
+    */
     observe(data, this);
+    
+    // observe(data, vm);
 
     this.$compile = new Compile(options.el || document.body, this)
 }
@@ -25,6 +76,7 @@ MVVM.prototype = {
     },
 
     _proxy: function(key) {
+      // key=>"msg" this=>vm对象
       // 保存vm以方便下面的函数中也可以找到vm
         var me = this;
 
@@ -42,5 +94,19 @@ MVVM.prototype = {
                 me._data[key] = newVal;
             }
         });
+
+        // 定义访问描述符(value值和get/set方法无法共存)
+      //   Object.defineProperty(vm, "msg", {
+      //     configurable: false, //不能重复定义
+      //     enumerable: true, //可以遍历
+      //     get: function proxyGetter() {
+      //       //当访问vm身上的属性值的时候，会去返回data的同名属性值
+      //         return vm._data["msg"];
+      //     },
+      //     set: function proxySetter(newVal) {
+      //       //当设置vm身上的属性值的时候，会去设置data的同名属性值
+      //         vm._data["msg"] = newVal;
+      //     }
+      // });
     }
 };
--- 3.复习/01.课件/Vue/Vue源码/js/observer.js
+++ 3.复习/01.课件/Vue/Vue源码/js/observer.js
@@ -1,5 +1,6 @@
 function Observer(data) {
-  // data就是我们传过来的data
+  // data就是我们传过来的data,vm._data
+//   this=>ob对象
   // 在Observer实例化对象身上先保存data 地址是一样的
     this.data = data;
     this.walk(data);//走起
@@ -13,13 +14,22 @@ Observer.prototype = {
             //这个方法内部又去调用了defineReactive方法
             me.convert(key, data[key]); //msg  'zhaoliying'
         });
+
+        // ["msg"].forEach(function(key) {
+        //     ob.convert("msg", "hello mvvm"); 
+        // });
     },
     convert: function(key, val) { //msg  'zhaoliying'
+        // "msg", "hello mvvm"
         this.defineReactive(this.data, key, val);  //整体的data  msg  'zhaoliying'
+        // this.defineReactive(vm._data, "msg", "hello mvvm");
     },
 
     defineReactive: function(data, key, val) { //整体的data  msg  'zhaoliying'
+        // vm._data, "msg", "hello mvvm"
         var dep = new Dep();  //根据遍历的每个属性都会创建一个特定的dep对象与相应的data属性进行关联
+
+        // 此处在递归进行数据劫持,对data中的所有属性进行深度递归
         var childObj = observe(val); //递归调用，目的是为了看属性的值是不是又是对象，如果是递归给每个属性也创建dep对象
         
         Object.defineProperty(data, key, {
@@ -47,6 +57,37 @@ Observer.prototype = {
                 dep.notify();
             }
         });
+
+        // 此处正在做数据劫持操作,正在重写_data身上的所有属性
+        // 将属性的value值替换成get和set方法
+        // 虽然Vue将所有属性都改成了get和set方法,会丢失value值,但是他通过闭包的方式缓存了当前的最新结果
+        // Object.defineProperty(vm._data, "msg", {
+        //     enumerable: true, // 可枚举
+        //     configurable: false, // 不能再define
+
+        //     //对data当中所有的属性都添加 get和set方法让其成为响应式的属性
+        //     //为了监视这些数据的变化，如果发生变化，那么将会通知去修改页面上的效果
+        //     get: function() {
+              
+        //         //让属性和页面上的表达式 进行绑定
+        //         if (Dep.target) {
+        //             dep.depend();
+        //         }
+        //         return val;
+        //     },
+        //     set: function(newVal) {
+            // 总结:如果旧值和新值完全相同,Vue不会更新视图
+        //         if (newVal === val) {
+        //             return;
+        //         }
+        //         val = newVal;
+        //         // 新的值是object的话，进行监听
+        //          Vue创建响应式属性的两个时机,一是初始化组件的data时候,二是当响应式属性的值发生变化,会重新劫持最新数值
+        //         childObj = observe(newVal);
+        //         // 通知订阅者
+        //         dep.notify();
+        //     }
+        // });
     }
 };
 
@@ -54,6 +95,8 @@ Observer.prototype = {
 //首先判断传过来的data也就是这个value是不是一个对象，如果是才去监视
 // {msg:'zhaoliying'}
 function observe(value, vm) {
+    // data, vm
+    // 此处在判断value值是否是对象,如果不是对象就停止执行
     if (!value || typeof value !== 'object') {
         return;
     }
