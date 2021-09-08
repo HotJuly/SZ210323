9e5bc2277936b073e2fbd2cbea6b01fe4cea19d1
HotJuly <949588380@qq.com>
2021-09-08 13:22:39 +0800

    53.React复习(二)

---
+++ 3.复习/01.课件/Webpack/2.tree shaking/src/lodash.js
@@ -0,0 +1,14 @@
+export function add(val1,val2){
+    console.log('add')
+    return val1 + val2;
+}
+
+export function reduce(val1,val2){
+    console.log('reduce')
+    return val1 - val2;
+}
+
+export function mulitp(val1,val2){
+    console.log('mulitp')
+    return val1 * val2;
+}
